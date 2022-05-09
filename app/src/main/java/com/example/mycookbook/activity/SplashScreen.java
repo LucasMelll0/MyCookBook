45f8924920bb76dbfb09f1ea3c37526a3ba0bfe1
Lucas Mello <lucasmellorodrigues2012@gmail.com>
2022-05-09 14:56:20 -0300

    Mudança na splash_screen.xml.
    allow_backup no AndroidManifest.xml agora esta como true.

--- app/src/main/java/com/example/mycookbook/activity/SplashScreen.java
+++ app/src/main/java/com/example/mycookbook/activity/SplashScreen.java
@@ -5,18 +5,25 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
 
 import com.example.mycookbook.R;
 
 public class SplashScreen  extends Activity {
 
     private static int TIME_OUT = 3000;
+    private AppCompatImageView imagemLogo;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        imagemLogo = findViewById(R.id.imageview_logo_splash_screen);
+        Animation animacao = AnimationUtils.loadAnimation(this, R.anim.rotate_animation);
+        imagemLogo.startAnimation(animacao);
 
         new Handler().postDelayed(new Runnable() {
             @Override
